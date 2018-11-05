72d4126697c8d3bd14f88cb3eb7567ce4003cd14
adwaith <adwaith1990@gmail.com>
2018-11-05 13:05:51 -0700

    Stack min by tracking minima along with the actual value as a tuple. Tuple = (value, minima upto that point). All operations are O(1)

---
+++ 3-stacks-and-queues/3.2-stack_min.py
@@ -0,0 +1,44 @@
+class Stack_Min():
+	stack=None
+	def __init__(self):
+		self.stack=list()
+		
+	def push(self, value):
+		if self.is_empty():
+			self.stack.append((value,value))
+		else:
+			if value<self.peek()[1]:
+				self.stack.append((value,value))
+			else:
+				self.stack.append((value, self.peek()[1]))
+				
+	def min(self):
+		return None if self.is_empty() else self.stack[-1][1]	
+				
+	def pop(self):
+		return None if self.is_empty() else	self.stack.pop()[0]		
+	
+	def is_empty(self):
+		return len(self.stack)==0	
+		
+	def peek(self):
+		return None if self.is_empty() else self.stack[-1]	
+				
+				
+if __name__=="__main__":
+	s=Stack_Min()
+	s.push(8)
+	s.push(4)
+	s.push(1)
+	s.push(9)
+	s.push(12)
+	s.push(0.5)
+	s.push(24)
+
+	s.pop()	
+	s.pop()
+	print(s.min())		
+	while s.peek():
+		print(s.pop())
+	
+	
