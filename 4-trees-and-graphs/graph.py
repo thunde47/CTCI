bd3c3a48cd14578803c6d70a5bc9c3bc2fa1c905
adwaith <adwaith1990@gmail.com>
2018-11-15 11:44:49 -0700

    Added BinaryNode class and pre-order-traversal

--- 4-trees-and-graphs/graph.py
+++ 4-trees-and-graphs/graph.py
@@ -1,6 +1,12 @@
 from stack import Stack
 from queue import Queue
 
+class BinaryNode:
+	def __init__(self, value):
+		self.value=value
+		self.right=None
+		self.left=None
+				
 class Graph:
 	def __init__(self, graph=dict()):
 		self.graph=graph
@@ -30,12 +36,23 @@ class Graph:
 						return False
 			else: return False			
 		return "Graph is a binary tree (at most two child nodes)"
-		
+	
+	def generate_binary_tree(self):
+		for key, connections in self.graph.items():
+			
+			node=BinaryNode(key)
+			try: 
+				node.left=BinaryNode(connections[0])
+				node.right=BinaryNode(connections[1])
+			except:pass
+			print(node.value, node.left.value, node.right.value)
+			
+			
 	def is_binary_search_tree(self):
 		pass
 		
 	#Root-->Left-->Right	
-	def pre_order_traversal(self, root): 
+	def pre_order_traversal(self, root):		
 		print(root)
 		try:
 			left=self.pre_order_traversal(self.graph[root][0])
@@ -44,13 +61,30 @@ class Graph:
 			if right!=None: print(right)
 		except:
 			pass	
+			
+def pre_order_traversal(node):
+	print(node.value)	
+	if node.left!=None:
+		pre_order_traversal(node.left)
+	if node.right!=None:
+		pre_order_traversal(node.right)
 		
+
 if __name__=="__main__":
 	g=Graph()
 	g.add_node(8,[4,10])
 	g.add_node(4,[2,6])
-	g.add_node(10,[12, 20])
+	g.add_node(10,[20])
 	print(g.is_tree())
 	print(g.is_binary_tree())
-	g.pre_order_traversal(8)
-	g.show()
+	
+	root=BinaryNode(8)
+	root.left=BinaryNode(4)
+	root.right=BinaryNode(10)
+	root.left.left=BinaryNode(2)
+	root.left.right=BinaryNode(6)
+	root.right.right=BinaryNode(20)
+
+	pre_order_traversal(root)
+	
+	
