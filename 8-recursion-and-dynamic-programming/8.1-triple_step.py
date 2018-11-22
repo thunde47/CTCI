477b32a34cfd53caf7f8472c22241a259b657c9b
adwaith <adwaith1990@gmail.com>
2018-11-22 13:41:26 -0700

    With caching complexity reduces to O(N)

--- 8-recursion-and-dynamic-programming/8.1-triple_step.py
+++ 8-recursion-and-dynamic-programming/8.1-triple_step.py
@@ -1,6 +1,8 @@
+import time
+
 p=0
 def steps(n): #O(3^N)
-	#print(n, p)
+	
 	if n==0:
 		return p+1
 	if n<=1:
@@ -10,6 +12,31 @@ def steps(n): #O(3^N)
 	
 	if n>=3:
 		return steps(n-1) + steps(n-2) + steps(n-3)
+		
+p=0		
+def steps_efficient(n): #O(N)
+	cache=[None]*(n+1)
+	return steps_cache(n, cache)
 	
-n=4
-print(steps(n))
+def steps_cache(n, cache):
+	if n==0:
+		return p+1
+	if cache[n]!=None:
+		return cache[n]
+	if n<=1:
+		cache[n]=steps_cache(n-1,cache)
+		return cache[n]
+	if n<=2:
+		cache[n]=steps_cache(n-1,cache) + steps_cache(n-2, cache)
+		return cache[n]
+	if n>=3:
+		cache[n]=steps_cache(n-1, cache) + steps_cache(n-2, cache) + steps_cache(n-3, cache)	
+		return cache[n]
+	
+n=30
+t0=time.time()
+print("Permutations using Brute force=",steps(n))
+print("Time elapsed", time.time()-t0)
+t0=time.time()
+print("Permutations using Caching=", steps_efficient(n))
+print("Time elapsed", time.time()-t0)
