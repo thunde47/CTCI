0d4c089d7337f014702632424058f5bcb00d8103
adwaith <adwaith1990@gmail.com>
2018-11-22 13:19:17 -0700

    Triple step without memoization, takes O(3^N) time

---
+++ 8-recursion-and-dynamic-programming/8.1-triple_step.py
@@ -0,0 +1,15 @@
+p=0
+def steps(n): #O(3^N)
+	#print(n, p)
+	if n==0:
+		return p+1
+	if n<=1:
+		return steps(n-1)
+	if n<=2:
+		return steps(n-1) + steps(n-2)
+	
+	if n>=3:
+		return steps(n-1) + steps(n-2) + steps(n-3)
+	
+n=0
+print(steps(n))
