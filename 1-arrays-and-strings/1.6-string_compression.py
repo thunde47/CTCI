5d821a4ca9f134f31a7ed61267c6ac0575cfaab6
adwaith <adwaith1990@gmail.com>
2018-11-26 20:20:37 -0700

    Checking the compressed string length on the fly, reducing time complexity to almost half on an average

--- 1-arrays-and-strings/1.6-string_compression.py
+++ 1-arrays-and-strings/1.6-string_compression.py
@@ -1,20 +1,24 @@
 def compression(S):
-  count=1
-  code=list()
+
   original_length=len(S)
   if original_length==0:
   	return S
+  count=1
+  code=list()
+  code_length=0
   for i in range(0, original_length-1): #O(N)
-    
+    if code_length>=original_length:
+    	return S
     if S[i]==S[i+1]:
       count+=1
     else:
       code.append(S[i])
       code.append(str(count))
+      code_length+=2
       count=1
   code.append(S[-1])
   code.append(str(count))
-  return S if original_length<=len(code) else ''.join(code)
+  return ''.join(code)
   
 if __name__=="__main__":
-	print(compression("aaaabbbbbaaaatttttttooopooopooojj"))
+	print(compression("abcddddddddddddddddddddddddacccooottt"))
